db6b5eab5e9d12faa404a14139c18d61fb90c3fb
Jason <77237605+Jio0oiJ@users.noreply.github.com>
2024-09-04 17:41:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -39,8 +39,8 @@ sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generat
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 rm -rf feeds/packages/net/adguardhome
 git clone https://github.com/Jio0oiJ/adguardhome feeds/packages/net/adguardhome
