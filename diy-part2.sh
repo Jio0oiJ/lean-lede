681311938c377b0eef3bb2ee653972c75beba27c
Jason <77237605+Jio0oiJ@users.noreply.github.com>
2024-09-05 11:31:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -52,7 +52,7 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
-git clone https://github.com/sbwml/luci-app-mosdns -b v5.3.3 package/mosdns
+git clone https://github.com/sbwml/luci-app-mosdns -b v5.3.1 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 curl -o package/mosdns/luci-app-mosdns/root/etc/mosdns/config_custom.yaml https://raw.githubusercontent.com/Jio0oiJ/mosdns_config/master/config_custom.yaml
 
